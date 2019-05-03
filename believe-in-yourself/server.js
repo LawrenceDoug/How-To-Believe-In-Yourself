09c3c633bd32800564bbe397fa52f4311d605f3e
Lawrence Douglas <ndcvlonesol03@gmail.com>
2019-05-03 07:30:10 -0600

    Fix Audit

--- believe-in-yourself/server.js
+++ believe-in-yourself/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname + '/src'));
 
 app.listen(process.env.PORT || 8080);
 
 // Path location strategy
 
-app.get('*', function(req, res) { 
-    res.sendFile(path.join(_dirname + 'index.html'));
+app.get('/*', function(req, res) { 
+    res.sendFile(path.join(_dirname + '/src/index.html'));
 });
 
 console.log('listening');
