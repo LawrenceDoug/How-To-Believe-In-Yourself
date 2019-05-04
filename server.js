5fb6baf2b14b4afad20228969fa2f488d13d63c4
Lawrence Douglas <ndcvlonesol03@gmail.com>
2019-05-03 19:01:28 -0600

    Deploying

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist/how-to-believe-in-yourself'));
 
 app.listen(process.env.PORT || 8080);
 
 // Path location strategy
 
 app.get('/*', function(req, res) { 
-    res.sendFile(path.join(_dirname + '/src/index.html'));
+    res.sendFile(path.join(_dirname + '/dist/how-to-believe-in-yourself/index.html'));
 });
 
 console.log('listening');
